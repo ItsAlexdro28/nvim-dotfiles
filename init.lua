ea7403e7fd9c8c07492a82b9b38d7ff667283999
ItsAlexdro28 <alejandroj5025@gmail.com>
2024-09-19 22:35:08 -0500

    idk what im doing.jgp :tada:

---
+++ init.lua
@@ -0,0 +1,31 @@
+-- Imported from old vimrc
+
+-- vim.cmd([[set number 
+-- set relativenumber 
+-- set mouse=a 
+-- set shiftwidth=4 
+-- set tabstop=4 
+-- set autoindent 
+-- set nocompatible 
+-- set nowrap]])
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.mouse = 'a'
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.autoindent = true
+vim.opt.wrap = true
+vim.g.have_nerd_font = true
+
+-- the lazy plugin installer
+require("config.lazy")
+require("config.lsp-zero")
+require("config.cmp")
+
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('tokyonight')
+
+
+
